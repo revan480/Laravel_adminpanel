11ce86c0b21ce4cfb781850a1fd769e4914e86b7
Ako2003 <akifmursalov@gmail.com>
2023-04-21 00:03:01 -0700

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Doctor as Doctor;
 
 class DatabaseSeeder extends Seeder
 {
