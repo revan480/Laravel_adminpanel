1232d3a6d25a1cd1482516b3bef181ac21567d68
Ako2003 <akifmursalov@gmail.com>
2023-04-23 01:06:19 -0700

    wip

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -10,4 +10,18 @@ class Patient extends Model
 {
     use CrudTrait;
     use HasFactory;
+
+    // Make rows fillable so that the could be changed
+    protected $fillable = [
+        'name',
+        'surname',
+        'phone',
+        'area',
+        'price',
+        'doctor_name',
+        'room_number',
+        'bill_type',
+        'feedback',
+        'date',
+    ];
 }
